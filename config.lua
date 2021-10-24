8796a34df23b36a48a7fdc897518879afde2e932
renzuzu <82306584+renzuzu@users.noreply.github.com>
2021-10-24 20:45:46 +0800

    config comments

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.Mysql = 'ghmattisql'
+Config.Mysql = 'mysql-async' -- mysql-async, ghmattisql, oxmysql
 Config.weight_type = false
 Config.weight = 1.5
 Config.Framework = 'ESX' -- ESX,QBCORE,Standalone (ITEM PURPOSE ONLY)
