6f0af13c866a8b232494df83847e6f2d8ca10ca6
xrtw <55210961+xrtw@users.noreply.github.com>
2022-12-20 16:12:50 +0200

    Choice, KVP or SQL

--- config.lua
+++ config.lua
@@ -5,6 +5,7 @@ Config.weight_type = true
 Config.weight = 1.5
 Config.Framework = 'Standalone' -- ESX,QBCORE,Standalone (ITEM PURPOSE ONLY)
 Config.FixedCamera = true
+Config.KVP = true -- storage KVP, false equals sql
 
 --COMMANDS AND KEYBINDS
 Config.commands = 'stance'
